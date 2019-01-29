fd27405ed26c608fe526a19124bbf732bf6beb91
Zulkar Nayem <nayem.cosmic@gmail.com>
2019-01-29 13:53:28 +0600

    Changed variable names for readablity

--- rc_boat_bluetooth.ino
+++ rc_boat_bluetooth.ino
@@ -6,14 +6,17 @@ Servo starboard;
 Servo bldc;
 SevSeg speedmeter;
 char command = 'S';
-int rservo = 12;
+int led = 13;
+int ledG = 7;
+int bldcPin = 12;
 int lservo = 11;
-int bldc = 10;
+int rservo = 10;
 int relayA = 9;
 int relayB = 8;
 int pulseTime = 1100;
-int posR = 80;
-int posL = 88;
+int posR = 76;
+int posL = 82;
+int pspeed = 00;
 
 
 void setup()
@@ -21,199 +24,173 @@ void setup()
   Serial.begin(9600);
   port.attach(lservo);
   starboard.attach(rservo);
-  bldc.attach(bldc);
+  bldc.attach(bldcPin);
   port.write(posL);
   starboard.write(posR);
-  pinMode(bldc, OUTPUT);
   pinMode(relayA, OUTPUT);
   pinMode(relayB, OUTPUT);
-  bldc.writeMicroseconds(1100); //bldc arming
-  
-  byte numDigits = 2; //speed display digits  
-  byte digitPins[] = {};
-  byte segmentPins[] = {};
-  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
-  sevseg.setBrightness(100);
+  delay(1);
+  bldc.writeMicroseconds(1000); //bldc arming
+
+  byte numDigits = 2; //speed display digits
+  byte digitPins[] = {38, 40}; //A,B,C,D,E,F,G pin
+  byte segmentPins[] = {36, 34, 32, 30, 28, 26, 24}; //digit-1 cathode, digit-2 cathode
+  speedmeter.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+  pinMode(led, OUTPUT);
+  pinMode(ledG, OUTPUT);
+  digitalWrite(led, HIGH);
+  digitalWrite(ledG, LOW);
 }
 
 void loop() {
   if (Serial.available() > 0) {
     command = Serial.read();
-    if (command == 'F') {
+    if (command == 'F') {    //move front
       digitalWrite(relayA, LOW);
       digitalWrite(relayB, LOW);
-      posL=88;
-      posR=80;
+      posL = 82;
+      posR = 76;
       port.write(posL);       //left servo
       starboard.write(posR);  //right servo
-      delay(100);
       bldc.writeMicroseconds(pulseTime);
     }
-    else if (command == 'B') {
+    else if (command == 'B') {    //move back
       digitalWrite(relayA, HIGH);
       digitalWrite(relayB, HIGH);
-      delay(10);
-      if(pulseTime!=1201){
-        bldc.writeMicroseconds(1000);
-        delay(50);
-        pulseTime= 1201;
-      }
+      posL = 82;
+      posR = 76;
+      port.write(posL);       //left servo
+      starboard.write(posR);  //right servo
       bldc.writeMicroseconds(pulseTime);
-        
     }
-    else if (command == 'L') {
+    else if (command == 'L') {   //move left, motor off
       posR += 5;
       posL += 5;
-      if (posR > 130)
-        posR = 130;
-      if (posL > 130)
-        posL = 130;
+      if (posR > 116)
+        posR = 116;
+      if (posL > 122)
+        posL = 122;
       starboard.write(posR);
       port.write(posL);
-      delay(50);
     }
-    else if (command == 'R') {
+    else if (command == 'R') {     //move right, motor off
       posR -= 5;
       posL -= 5;
-      if (posR < 50)
-        posR = 50;
-      if (posL < 50)
-        posL = 50;
+      if (posR < 36)
+        posR = 36;
+      if (posL < 42)
+        posL = 42;
       starboard.write(posR);
       port.write(posL);
-      delay(50);
     }
-    else if (command == 'S') {
+    else if (command == 'S') {    //default
       digitalWrite(relayA, LOW);
       digitalWrite(relayB, LOW);
-      posR = 80;
-      posL = 88;
+      posR = 76;
+      posL = 82;
       starboard.write(posR);
       port.write(posL);
-      delay(10);
       bldc.writeMicroseconds(1000);
     }
-    else if (command == 'I') {
+    else if (command == 'I') {    //move right, motor on
+      digitalWrite(relayA, LOW);
+      digitalWrite(relayB, LOW);
       bldc.writeMicroseconds(pulseTime);
       posR -= 5;
       posL -= 5;
-      if (posR < 50)
-        posR = 50;
-      if (posL < 50)
-        posL = 50;
+      if (posR < 36)
+        posR = 36;
+      if (posL < 42)
+        posL = 42;
       starboard.write(posR);
       port.write(posL);
-      delay(50);
     }
-    else if (command == 'J') {
+    else if (command == 'J') {    //move back-right
       digitalWrite(relayA, HIGH);
       digitalWrite(relayB, HIGH);
-      delay(10);
-      if(pulseTime!=1201){
-        bldc.writeMicroseconds(1000);
-        delay(50);
-        pulseTime= 1201;
-      }
       bldc.writeMicroseconds(pulseTime);
       posR -= 5;
       posL -= 5;
-      if (posR < 50)
-        posR = 50;
-      if (posL < 50)
-        posL = 50;
+      if (posR < 36)
+        posR = 36;
+      if (posL < 42)
+        posL = 42;
       starboard.write(posR);
       port.write(posL);
-      delay(50);
-
     }
-    else if (command == 'G') {
-      esc(pulseTime);
-      posR += 5;
-      posL += 5;
-      if (posR > 130)
-        posR = 130;
-      if (posL > 130)
-        posL = 130;
-      starboard.write(posR);
-      port.write(posL);
-      delay(50);
-    }
-    else if (command == 'H') {
-      digitalWrite(relayA, HIGH);
-      digitalWrite(relayB, HIGH);
-      delay(10);
-      if(pulseTime!=1201){
-        bldc.writeMicroseconds(1000);
-        delay(50);
-        pulseTime= 1201;
-      }
+    else if (command == 'G') {    //move left, motor on
+      digitalWrite(relayA, LOW);
+      digitalWrite(relayB, LOW);
       bldc.writeMicroseconds(pulseTime);
       posR += 5;
       posL += 5;
-      if (posR > 130)
-        posR = 130;
-      if (posL > 130)
-        posL = 130;
+      if (posR > 116)
+        posR = 116;
+      if (posL > 122)
+        posL = 122;
       starboard.write(posR);
       port.write(posL);
-      delay(50);
     }
+    else if (command == 'H') {    //move back-left
+      digitalWrite(relayA, HIGH);
+      digitalWrite(relayB, HIGH);
+      bldc.writeMicroseconds(pulseTime);
+      posR += 5;
+      posL += 5;
+      if (posR > 116)
+        posR = 116;
+      if (posL > 122)
+        posL = 122 ;
+      starboard.write(posR);
+      port.write(posL);
+    }
+    //set speed
     else if (command == '0') {
       pulseTime = 1100;
-      speedmeter.setNumber(00,0);
-      speedmeter.refreshDisplay();
+      pspeed = 00;
     }
     else if (command == '1') {
-      pulseTime = 1170;
-      speedmeter.setNumber(10,0);
-      speedmeter.refreshDisplay();
+      pulseTime = 1190;
+      pspeed = 10;
     }
     else if (command == '2') {
       pulseTime = 1200;
-      speedmeter.setNumber(20,0);
-      speedmeter.refreshDisplay();
+      pspeed = 20;
     }
     else if (command == '3') {
-      pulseTime = 1300;
-      speedmeter.setNumber(30,0);
-      speedmeter.refreshDisplay();
+      pulseTime = 1225;
+      pspeed = 30;
     }
     else if (command == '4') {
-      pulseTime = 1400;
-      speedmeter.setNumber(40,0);
-      speedmeter.refreshDisplay();
+      pulseTime = 1250;
+      pspeed = 40;
     }
     else if (command == '5') {
-      pulseTime = 1500;
-      speedmeter.setNumber(50,0);
-      speedmeter.refreshDisplay();
+      pulseTime = 1275;
+      pspeed = 50;
     }
     else if (command == '6') {
-      pulseTime = 1600;
-      speedmeter.setNumber(60,0);
-      speedmeter.refreshDisplay();
+      pulseTime = 1300;
+      pspeed = 60;
     }
     else if (command == '7') {
-      pulseTime = 1700;
-      speedmeter.setNumber(70,0);
-      speedmeter.refreshDisplay();
+      pulseTime = 1400;
+      pspeed = 70;
     }
     else if (command == '8') {
-      pulseTime = 1800;
-      speedmeter.setNumber(80,0);
-      speedmeter.refreshDisplay();
+      pulseTime = 1500;
+      pspeed = 80;
     }
     else if (command == '9') {
-      pulseTime = 1900;
-      speedmeter.setNumber(90,0);
-      speedmeter.refreshDisplay();
+      pulseTime = 1700;
+      pspeed = 90;
     }
     else if (command == 'q') {
-      pulseTime = 2000;
-      speedmeter.setNumber(99,0);
-      speedmeter.refreshDisplay();
+      pulseTime = 1800 ;
+      pspeed = 99;
     }
-
   }
+  speedmeter.refreshDisplay();
+  speedmeter.setNumber(pspeed, 2);
 }
 
